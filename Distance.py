3eead082ef0d04c5058a6b913c1e3d2d78cf4210
BJCodeHub <148155316+BJCodeHub@users.noreply.github.com>
2023-11-21 19:39:24 +0200

    Update Distance.py

--- Distance.py
+++ Distance.py
@@ -20,8 +20,12 @@ if req_conv == 'miles' and req_converted == 'kilometers':
     print (conv)
 
 # Instead you can use this simple code
-req_conv = str(input('From {length_conv}; '))
-req_converted = str(input('To {length_conv}: '))
+length_conv =  (
+'miles', 'kilometers', 'meters', 'inches', 'centimeters', 'millimeters', 'yards', 'parsecs', 'INM'
+    )
+
+req_conv = str(input(f'From {length_conv}: '))
+req_converted = str(input(f'To {length_conv}: '))
 
 conversions = {
     ('kilometers', 'miles'): 0.621371,
@@ -34,6 +38,10 @@ conversions = {
     ('inches', 'millimeters'): 25.4,
     ('yards', 'meters'): 0.9144,
     ('meters', 'yards'): 1.09361,
+    ('parsecs', 'meters'): 3.0857e+16,
+    ('meters', 'parsecs'): 3.2408e-17,
+    ('INM', 'meters'): 1852,
+    ('meters', 'INM'): 0.000539957
 }
 
 def convert_distance(distance, unit_1, unit_2):
@@ -42,9 +50,9 @@ def convert_distance(distance, unit_1, unit_2):
     if conversion is None:
         print("Invalid conversion units")
         return None
-    return distance * conversion
+    return float(distance) * conversion
 
 distance = float(input ('Distance: '))
 conversion_distance = convert_distance(distance, req_conv, req_converted)
-if convert_distance is not None:
+if conversion_distance is not None:
     print(f"{distance} {req_conv} is equals to {conversion_distance} {req_converted}")
